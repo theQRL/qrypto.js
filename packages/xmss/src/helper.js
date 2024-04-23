fbd670308cac8f3b5d3c21054580cad4dfe87a39
Shaheer Shukur <shaheershukur@gmail.com>
2024-04-23 18:59:41 +0400

    feat: adds shake256 utility and test cases

--- packages/xmss/src/helper.js
+++ packages/xmss/src/helper.js
@@ -1,14 +1,17 @@
-import { shake256 } from 'js-sha3';
+import jsSha3CommonJsPackage from 'js-sha3';
+const { shake256: sha3Shake256 } = jsSha3CommonJsPackage;
 
 /**
  * @param {Uint8Array} out
- * @param {Uint8Array} msg
+ * @param {Uint8Array} message
  * @returns {Uint8Array}
  */
-export function SHAKE256(out, msg) {
-  let hasherString = shake256(msg, out[0]);
-  const encoder = new TextEncoder();
-  const utf8Bytes = encoder.encode(hasherString);
-  const outUint8Array = new Uint8Array(utf8Bytes);
-  return outUint8Array;
+export function shake256(out, message) {
+  const outUInt8Length = out.length * 8;
+  const hash = sha3Shake256(message, outUInt8Length);
+
+  for (let i = 0; i < outUInt8Length; i += 2) {
+    out[i / 2] = parseInt(hash.substring(i, i + 2), 16);
+  }
+  return out;
 }
--- packages/xmss/test/helper.test.js
+++ packages/xmss/test/helper.test.js
@@ -1,9 +1,45 @@
 import { expect } from 'chai';
+import { shake256 } from '../src/helper.js';
 
-describe('helper', () => {
-  it('invalid key length in polyVecLUniformEta throws', () => {
-    expect(() => {
-      polyVecLUniformEta(1, 2, 3);
-    }).to.throw();
+describe('shake256', () => {
+  it('should return the SHAKE256 hash of type Uint8Array', () => {
+    const message = new Uint8Array(48);
+    let out = new Uint8Array(18);
+    out = shake256(out, message);
+
+    expect(out).to.be.an.instanceOf(Uint8Array);
+  });
+
+  it('should return the SHAKE256 hashed Uint8Array with message[48] and out[18]', () => {
+    const message = new Uint8Array(48);
+    let out = new Uint8Array(18);
+    const expectedShake256Out = new Uint8Array([
+      237, 163, 19, 201, 85, 145, 160, 35, 165, 179, 127, 54, 28, 7, 165, 117, 58, 146,
+    ]);
+    out = shake256(out, message);
+
+    expect(out).to.deep.equal(expectedShake256Out);
+  });
+
+  it('should return the SHAKE256 hashed Uint8Array with message[12] and out[6]', () => {
+    const message = new Uint8Array(12);
+    message[0] = 5;
+    let out = new Uint8Array(6);
+    out[0] = 3;
+    const expectedShake256Out = new Uint8Array([119, 94, 102, 126, 219, 180]);
+    out = shake256(out, message);
+
+    expect(out).to.deep.equal(expectedShake256Out);
+  });
+
+  it('should return the SHAKE256 hashed Uint8Array with message[54] and out[15]', () => {
+    const message = new Uint8Array(54);
+    message[5] = 7;
+    let out = new Uint8Array(15);
+    out[3] = 12;
+    const expectedShake256Out = new Uint8Array([206, 80, 114, 99, 195, 185, 167, 206, 248, 101, 163, 95, 103, 29, 151]);
+    out = shake256(out, message);
+
+    expect(out).to.deep.equal(expectedShake256Out);
   });
 });
