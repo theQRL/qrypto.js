ccc05d6eb2b5cd95d6ebf639c3d28031e1ae62ba
Shaheer Shukur <shaheershukur@gmail.com>
2024-04-23 13:12:10 +0400

    feat: configures jsdoc and sha3 library

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,7 +1,8 @@
 module.exports = {
-  trailingComma: "es5",
+  plugins: ['prettier-plugin-jsdoc'],
+  trailingComma: 'es5',
   tabWidth: 2,
   semi: true,
   singleQuote: true,
   printWidth: 120,
-}
+};
