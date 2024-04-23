79ee6b478c97698aa09014f8ca3bed54c8d257eb
Shaheer Shukur <shaheershukur@gmail.com>
2024-04-23 18:55:45 +0400

    refactor: skips test

--- packages/xmss/test/xmssFast.test.js
+++ packages/xmss/test/xmssFast.test.js
@@ -1,10 +1,10 @@
-import { expect } from 'chai';
-import { polyVecLUniformEta } from '../src/polyvec.js';
+// import { expect } from 'chai';
+// import { polyVecLUniformEta } from '../src/polyvec.js';
 
-describe('xmssFast', () => {
-  it('invalid key length in polyVecLUniformEta throws', () => {
-    expect(() => {
-      polyVecLUniformEta(1, 2, 3);
-    }).to.throw();
-  });
-});
+// describe('xmssFast', () => {
+//   it('invalid key length in polyVecLUniformEta throws', () => {
+//     expect(() => {
+//       polyVecLUniformEta(1, 2, 3);
+//     }).to.throw();
+//   });
+// });
