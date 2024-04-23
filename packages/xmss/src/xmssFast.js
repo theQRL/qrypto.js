038d87a7df1e804e137ad2ee7f0c6240cda976c0
Shaheer Shukur <shaheershukur@gmail.com>
2024-04-24 02:41:39 +0400

    feat: adds genLeafWOTS function

--- packages/xmss/src/xmssFast.js
+++ packages/xmss/src/xmssFast.js
@@ -34,7 +34,27 @@ export function XMSSFastGenKeyPair(hashFunction, xmssParams, pk, sk, bdsState, s
   pk.set(sk.subarray(4 + 2 * n, 4 + 2 * n + pks), n);
 
   const addr = new Uint32Array(8);
+  // TODO:
   // treeHashSetup(hashFunction, pk, 0, bdsState, sk[4:4+n], xmssParams, sk[4+2*n:4+2*n+n], addr)
 
   sk.set(pk.subarray(0, pks), 4 + 3 * n);
+}
+
+/**
+ * @param {HashFunction} hashFunction
+ * @param {Uint8Array} leaf
+ * @param {Uint8Array} skSeed
+ * @param {XMSSParams} xmssParams
+ * @param {Uint8Array} pubSeed
+ * @param {Uint32Array} lTreeAddr
+ * @param {Uint32Array} otsAddr
+ */
+export function genLeafWOTS(hashFunction, leaf, skSeed, xmssParams, pubSeed, lTreeAddr, otsAddr) {
+  const seed = new Uint8Array(xmssParams.n);
+  const pk = new Uint8Array(xmssParams.wotsParams.keySize);
+
+  // TODO:
+  // getSeed(hashFunction, seed, skSeed, xmssParams.n, otsAddr)
+  // wOTSPKGen(hashFunction, pk, seed, xmssParams.wotsParams, pubSeed, otsAddr)
+  // lTree(hashFunction, xmssParams.wotsParams, leaf, pk, pubSeed, lTreeAddr)
 }
