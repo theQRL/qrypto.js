64240f2567caaeefd34bf1e6d6430e3e7cd18967
Shaheer Shukur <shaheershukur@gmail.com>
2024-04-26 20:46:23 +0400

    refactor: starts hashH function

--- packages/xmss/src/hash.js
+++ packages/xmss/src/hash.js
@@ -1,7 +1,7 @@
 /// <reference path="typedefs.js" />
 
 import { HASH_FUNCTION } from './constants.js';
-import { sha256, shake128, shake256, toByteLittleEndian } from './helper.js';
+import { addrToByte, setKeyAndMask, sha256, shake128, shake256, toByteLittleEndian } from './helper.js';
 
 /**
  * @param {HashFunction} hashFunction
@@ -51,4 +51,40 @@ export function coreHash(hashFunction, out, typeValue, key, keyLen, input, inLen
  */
 export function prf(hashFunction, out, input, key, keyLen) {
   return coreHash(hashFunction, out, 3, key, keyLen, input, 32, keyLen);
+}
+
+// TODO: Once all objects are modified as reference, complete this.
+/**
+ * @param {HashFunction} hashFunction
+ * @param {Uint8Array} out
+ * @param {Uint8Array} input
+ * @param {Uint8Array} pubSeed
+ * @param {Uint32Array} addr
+ * @param {Uint32Array[number]} n
+ * @returns {Uint8Array}
+ */
+export function hashH(hashFunction, out, input, pubSeed, addr, n) {
+  let addrValue = addr;
+  const buf = new Uint8Array(2 * n);
+  let key = new Uint8Array(n);
+  const bitMask = new Uint8Array(2 * n);
+  let byteAddr = new Uint8Array(32);
+
+  addrValue = setKeyAndMask(addrValue, 0);
+  byteAddr = addrToByte(byteAddr, addrValue);
+  key = prf(hashFunction, key, byteAddr, pubSeed, n);
+
+  // Use MSB order
+  addrValue = setKeyAndMask(addrValue, 1);
+  byteAddr = addrToByte(byteAddr, addrValue);
+  bitMask = prf(hashFunction, bitMask.subarray(0, n), byteAddr, pubSeed, n);
+
+  // prf(hashFunction, bitMask[:n], byteAddr[:], pubSeed, n)
+  // misc.SetKeyAndMask(addr, 2)
+  // misc.AddrToByte(&byteAddr, addr)
+  // prf(hashFunction, bitMask[n:n+n], byteAddr[:], pubSeed, n)
+  // for i := uint32(0); i < 2*n; i++ {
+  // 	buf[i] = input[i] ^ bitMask[i]
+  // }
+  // coreHash(hashFunction, out, 1, key, n, buf, 2*n, n)
 }
