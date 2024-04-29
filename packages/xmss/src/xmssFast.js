32f30b17c623843382287d2e5c9d7ac4a2543c1d
Shaheer Shukur <shaheershukur@gmail.com>
2024-04-29 17:01:09 +0400

    feat: adds getSeed function and test

--- packages/xmss/src/xmssFast.js
+++ packages/xmss/src/xmssFast.js
@@ -36,27 +36,7 @@ export function XMSSFastGenKeyPair(hashFunction, xmssParams, pk, sk, bdsState, s
 
   const addr = new Uint32Array(8);
   // treeHashSetup(hashFunction, pk, 0, bdsState, sk[4:4+n], xmssParams, sk[4+2*n:4+2*n+n], addr)
-
   sk.set(pk.subarray(0, pks), 4 + 3 * n);
-}
-
-/**
- * @param {HashFunction} hashFunction
- * @param {Uint8Array} leaf
- * @param {Uint8Array} skSeed
- * @param {XMSSParams} xmssParams
- * @param {Uint8Array} pubSeed
- * @param {Uint32Array} lTreeAddr
- * @param {Uint32Array} otsAddr
- */
-export function genLeafWOTS(hashFunction, leaf, skSeed, xmssParams, pubSeed, lTreeAddr, otsAddr) {
-  const seed = new Uint8Array(xmssParams.n);
-  const pk = new Uint8Array(xmssParams.wotsParams.keySize);
-
-  // TODO:
-  // getSeed(hashFunction, seed, skSeed, xmssParams.n, otsAddr)
-  // wOTSPKGen(hashFunction, pk, seed, xmssParams.wotsParams, pubSeed, otsAddr)
-  // lTree(hashFunction, xmssParams.wotsParams, leaf, pk, pubSeed, lTreeAddr)
 }
 
 /**
@@ -76,4 +56,23 @@ export function getSeed(hashFunction, seed, skSeed, n, addr) {
   // // Generate pseudorandom value
   addrToByte(bytes, addr);
   prf(hashFunction, seed, bytes, skSeed, n);
+}
+
+/**
+ * @param {HashFunction} hashFunction
+ * @param {Uint8Array} leaf
+ * @param {Uint8Array} skSeed
+ * @param {XMSSParams} xmssParams
+ * @param {Uint8Array} pubSeed
+ * @param {Uint32Array} lTreeAddr
+ * @param {Uint32Array} otsAddr
+ */
+export function genLeafWOTS(hashFunction, leaf, skSeed, xmssParams, pubSeed, lTreeAddr, otsAddr) {
+  const seed = new Uint8Array(xmssParams.n);
+  const pk = new Uint8Array(xmssParams.wotsParams.keySize);
+
+  getSeed(hashFunction, seed, skSeed, xmssParams.n, otsAddr);
+  // TODO:
+  // wOTSPKGen(hashFunction, pk, seed, xmssParams.wotsParams, pubSeed, otsAddr)
+  // lTree(hashFunction, xmssParams.wotsParams, leaf, pk, pubSeed, lTreeAddr)
 }
--- packages/xmss/test/xmssFast.test.js
+++ packages/xmss/test/xmssFast.test.js
@@ -1,4 +1,7 @@
+import { expect } from 'chai';
 import { describe, it } from 'mocha';
+import { HASH_FUNCTION } from '../src/constants.js';
+import { getSeed } from '../src/xmssFast.js';
 
 describe('xmssFast', () => {
   xdescribe('XMSSFastGenKeyPair', () => {
@@ -9,7 +12,47 @@ describe('xmssFast', () => {
     it('TODO', () => {});
   });
 
-  xdescribe('getSeed', () => {
-    it('TODO', () => {});
+  describe('getSeed', () => {
+    it('should update the seed variable with hashFunction SHA2_256', () => {
+      const seed = new Uint8Array([2, 3, 5, 7, 4, 9, 1, 0]);
+      getSeed(
+        HASH_FUNCTION.SHA2_256,
+        seed,
+        new Uint8Array([2, 5, 1, 9, 4, 9, 1, 0]),
+        1,
+        new Uint32Array([3, 0, 0, 0, 0, 0, 2, 8])
+      );
+      const expectedSeed = new Uint8Array([220, 249, 92, 97, 226, 29, 208, 118]);
+
+      expect(seed).to.deep.equal(expectedSeed);
+    });
+
+    it('should update the seed variable with hashFunction SHAKE_128', () => {
+      const seed = new Uint8Array([2, 3, 5, 7, 4, 9, 1, 0]);
+      getSeed(
+        HASH_FUNCTION.SHAKE_128,
+        seed,
+        new Uint8Array([2, 5, 1, 9, 4, 9, 1, 0]),
+        1,
+        new Uint32Array([3, 0, 0, 0, 0, 0, 2, 8])
+      );
+      const expectedSeed = new Uint8Array([52, 91, 189, 158, 58, 60, 154, 95]);
+
+      expect(seed).to.deep.equal(expectedSeed);
+    });
+
+    it('should update the seed variable with hashFunction SHAKE_256', () => {
+      const seed = new Uint8Array([2, 3, 5, 7, 4, 9, 1, 0]);
+      getSeed(
+        HASH_FUNCTION.SHAKE_256,
+        seed,
+        new Uint8Array([2, 5, 1, 9, 4, 9, 1, 0]),
+        1,
+        new Uint32Array([3, 0, 0, 0, 0, 0, 2, 8])
+      );
+      const expectedSeed = new Uint8Array([28, 88, 226, 254, 193, 12, 174, 167]);
+
+      expect(seed).to.deep.equal(expectedSeed);
+    });
   });
 });
