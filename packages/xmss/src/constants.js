48651e8ccaf22a06656bb90edaa5acff2c5bf3f4
Shaheer Shukur <shaheershukur@gmail.com>
2024-05-15 10:46:24 +0400

    refactor: freezes contant objects

--- packages/xmss/src/constants.js
+++ packages/xmss/src/constants.js
@@ -1,17 +1,17 @@
-export const ENDIAN = {
+export const ENDIAN = Object.freeze({
   LITTLE: 0,
   BIG: 1,
-};
+});
 
-export const HASH_FUNCTION = {
+export const HASH_FUNCTION = Object.freeze({
   SHA2_256: 0,
   SHAKE_128: 1,
   SHAKE_256: 2,
-};
+});
 
-export const COMMON = {
+export const COMMON = Object.freeze({
   DESCRIPTOR_SIZE: 3,
   ADDRESS_SIZE: 20,
   SEED_SIZE: 48,
   EXTENDED_SEED_SIZE: 51,
-};
+});
