83cc80879442d28d8ca5cab29129eecfce99cb98
Shaheer Shukur <shaheershukur@gmail.com>
2024-05-29 21:11:19 +0400

    fix: exports wordlist in index file

--- packages/xmss/src/index.js
+++ packages/xmss/src/index.js
@@ -2,5 +2,6 @@ export * from './classes.js';
 export * from './constants.js';
 export * from './hash.js';
 export * from './helper.js';
+export { default as WORD_LIST } from './wordList.js';
 export * from './xmss.js';
 export * from './xmssFast.js';
