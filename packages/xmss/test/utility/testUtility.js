120bb8982b93243c7b59e11243e3980a520d347e
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-19 12:24:29 +0400

    refactor: cleans up test utility function

--- packages/xmss/test/utility/testUtility.js
+++ packages/xmss/test/utility/testUtility.js
@@ -57,7 +57,6 @@ export const getHexFromUIntArray = (uIntArray, variant) => {
   const hexString = Array.from(uIntArray)
     .map((byte) => byte.toString(16).padStart(charSize, '0'))
     .join('');
-  console.log('>>>hexString: ', hexString);
 
   return hexString;
 };
@@ -72,12 +71,3 @@ export const getRecreatedUIntArray = (uIntArray, variant) => {
   const recreatedUIntArray = getUIntArrayFromHex(hexString, variant);
   return recreatedUIntArray;
 };
-
-getHexFromUIntArray(
-  new Uint8Array([
-    102, 25, 153, 94, 80, 214, 241, 97, 162, 182, 144, 99, 214, 38, 231, 227, 119, 188, 178, 202, 22, 56, 171, 125, 111,
-    0, 211, 152, 129, 100, 89, 132, 105, 56, 52, 86, 112, 147, 92, 125, 232, 52, 36, 136, 247, 132, 140, 97, 32, 216,
-    217, 65, 247, 236, 104, 107, 3, 57, 23, 172, 136, 102, 73, 78, 88, 47, 212,
-  ]),
-  UINT[8]
-);
